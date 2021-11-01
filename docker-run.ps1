1d61dc3be763008cfe76a246731452a5c85c4308
Marco Dittamondi <marco.dittamondi@gmail.com>
2021-11-01 22:11:36 +0100

    Rate limiting

--- docker-run.ps1
+++ docker-run.ps1
@@ -4,5 +4,6 @@ docker run -it --rm my-haproxy haproxy -c -f /usr/local/etc/haproxy/haproxy.cfg
 
 docker run -p 4000:4000 -p 7000:7000 --name my-running-haproxy my-haproxy
 
-#open browser http://localhost:4000/api/data
 #open browser http://localhost:7000/static/
+#open browser http://localhost:4000/api/data 10 times and count occurences of server1 and server2
+#open browser http://localhost:4000/api/data reload as fast as you can the page using F5
